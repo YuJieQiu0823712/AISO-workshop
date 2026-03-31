4971c708166ca9b3ca311c3cb884ad87fbd7e93e
Yu-Jie <chiou.jade@gmail.com>
2026-03-31 20:36:42 +0200

    add calculator

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -4,15 +4,22 @@ The `root_agent` is used to evaluate your agent's performance.
 """
 
 from google.adk.agents import llm_agent
+from my_agent.tools.calculator import calculator_tool
 
 root_agent = llm_agent.Agent(
-    model="gemini-2.5-flash-lite",
+    model="gemini-2.5-flash",
     name="agent",
     description="A helpful assistant.",
     instruction=(
         "You are a helpful assistant that answers "
-        "questions directly and concisely."
+        "questions directly and concisely. "
+        "Explain your reasoning step by step."
+        "Use the calculator tool when the user asks for numerical arithmetic calculations."
     ),
-    tools=[],
+    tools=[calculator_tool],
     sub_agents=[],
 )
+
+if __name__ == "__main__":
+    result=calculator_tool("add", 1, 2)
+    print(result)
--- my_agent/tools/calculator.py
+++ my_agent/tools/calculator.py
@@ -1,3 +1,18 @@
-def calculator(operation: str, a: float, b: float) -> str:
+def calculator_tool(operation: str, a: float, b: float) -> str:
     """A calculator tool that performs basic arithmetic."""
-    pass
+    if operation == "add":
+        return str(a + b)
+    elif operation == "subtract":
+        return str(a - b)
+    elif operation == "multiply":
+        return str(a * b)
+    elif operation == "divide":
+        if b == 0:
+            return "Error: Division by zero"
+        return str(a / b)
+    elif operation == "pow":
+        return str(a ** b)
+    elif operation == "sqrt":
+        return str(a ** 0.5)
+    else:
+        return "Error: Invalid operation"
